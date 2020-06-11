e7dc90a3cda40a348582adb32ec830e59571aee8
saki10 <rsp10111011@gmail.com>
2020-06-11 18:24:03 +0900

    chat-space

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,26 +16,24 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-
-    def edit
-      @group = Group.find(params[:id])
-    end
-
-    def update
-      @group = Group.find(params[:id])
-      if @group.update(group_params)
-        redirect_to root_path, notice: 'グループを更新しました'
-      else
-        render :edit
-      end
-    end
-
-    private
-    def group_params
-      params.require(:group).permit(:name, user_ids: [])
-    end
-   
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
 
 end
