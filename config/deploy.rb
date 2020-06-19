ec7ab3ff9e73e6226749c80ff3951bb29b84c8b4
saki10 <rsp10111011@gmail.com>
2020-06-19 21:20:49 +0900

    add-set

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "chat-space"
-repo_url,  'git@github.com:saki10/chat-spce.git'
+set :repo_url, 'git@github.com:saki10/chat-spce.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
